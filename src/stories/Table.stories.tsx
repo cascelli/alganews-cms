49cd0b6dcff194c8275d66b2b00eced1f25a555c
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2021-12-14 14:52:05 -0300

    4.24. Recuperando dados além do accessor

--- src/stories/Table.stories.tsx
+++ src/stories/Table.stories.tsx
@@ -32,14 +32,31 @@ Default.args = {
 };
 */
 
+/*
 type Data = {
   preview: React.ReactNode;
   col1: string,
   col2: string,
   actions: string
 }
+*/
+
+type Post = {
+  id: number
+  title: string
+  views: number
+  author: {
+    name: string
+    avatar: string
+  }
+  conversions: {
+    thousands: number
+    percentage: number
+  }
+}
 
 
+/*
 export function Default() {
 
   const data = useMemo<Data[]>(
@@ -111,9 +128,62 @@ export function Default() {
     []
  
   )
+*/
 
-    
-  const columns = useMemo<Column<Data>[]>(
+export function Default() {
+
+  const data = useMemo<Post[]>(
+
+    () => [
+
+      {
+        author: {
+          name: 'Daniel Bonifacio',
+          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNf0vAZLggJoZxGKpfOa3EBClHkwQmmvv9Lg&usqp=CAU'
+        },
+        id: 1,
+        conversions: {
+          percentage: 64.35,
+          thousands: 607,
+        },
+        title: 'Como dobrei meu salário aprendendo somente React',
+        views: 985415
+      },
+      {
+        author: {
+          name: 'Daniel Bonifacio',
+          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNf0vAZLggJoZxGKpfOa3EBClHkwQmmvv9Lg&usqp=CAU'
+        },
+        id: 2,
+        conversions: {
+          percentage: 64.35,
+          thousands: 607,
+        },
+        title: 'React.js vs. React Native: a REAL diferença entre os dois',
+        views: 985415
+      },
+      {
+        author: {
+          name: 'Daniel Bonifacio',
+          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNf0vAZLggJoZxGKpfOa3EBClHkwQmmvv9Lg&usqp=CAU'
+        },
+        id: 3,
+        conversions: {
+          percentage: 95.35,
+          thousands: 845,
+        },
+        title: 'Como dobrei meu salário aprendendo somente React',
+        views: 985415
+      } 
+
+    ],
+ 
+    []
+ 
+  )
+
+
+  const columns = useMemo<Column<Post>[]>(
 
     () => [
  
@@ -121,36 +191,62 @@ export function Default() {
  
         Header: '',
  
-        accessor: 'preview', // accessor is the "key" in the data
+        accessor: 'id', // accessor is the "key" in the data
+
+        Cell: () => <Icon path={ mdiOpenInNew } size={'14px'} color={'#09f'} />
  
       },
  
       {
  
-        Header: 'Column 1',
+        Header: 'Artigo',
  
-        accessor: 'col1', // accessor is the "key" in the data
+        accessor: 'title', // accessor is the "key" in the data
 
-        Cell: (row) => <div style={{textAlign: 'right'}}>{row.value}</div>
+        width: 320,
+
+        Cell: (props) => <div style={{textAlign: 'left', display: 'flex', gap: 8, alignItems: 'center'}}>
+          <img width={24} height={24} src={props.row.original.author.avatar} alt={props.row.original.author.name} />
+          {props.value}
+        </div>
  
       },
  
       {
  
-        Header: 'Column 2',
+        Header: 'Views',
  
-        accessor: 'col2',
+        accessor: 'views',
 
-        Cell: (row) => <div style={{textAlign: 'center'}}>{row.value}</div>
+        Cell: (props) => <div style={{textAlign: 'right', fontWeight: 700, fontFamily: '"Roboto mono", monospace'}}>{props.value.toLocaleString('pt-br')}</div>
 
  
       },
  
+      {
+ 
+        Header: 'Conversões',
+ 
+        accessor: 'conversions',
+
+        Cell: (props) => <div style={{display: 'flex', gap: 8, fontWeight: 700, fontFamily: '"Roboto mono", monospace'}}>
+          
+            <span>{props.value.thousands}k</span>
+            <span style={{ color: '#09f'}}>{props.value.percentage}%</span>
+
+          </div>
+ 
+      },
+
       {
  
         Header: 'Ações',
- 
-        accessor: 'actions',
+
+        Cell: (props) => <div style={{textAlign: 'right'}}>
+          
+            todo: actions
+
+          </div>
  
       },
 
@@ -161,16 +257,16 @@ export function Default() {
   )
 
  
-  const instance = useTable<Data>({ data, columns })
+  const instance = useTable<Post>({ data, columns })
 
-  return <Table<Data> instance={instance} />
+  return <Table<Post> instance={instance} />
 
 }
 
 
 export function NoData() {
 
-  const data = useMemo<Data[]>(
+  const data = useMemo<Post[]>(
 
     () => [],
     []
@@ -178,7 +274,7 @@ export function NoData() {
   )
 
     
-  const columns = useMemo<Column<Data>[]>(
+  const columns = useMemo<Column<Post>[]>(
 
     () => [
  
@@ -226,8 +322,8 @@ export function NoData() {
   )
 
  
-  const instance = useTable<Data>({ data, columns })
+  const instance = useTable<Post>({ data, columns })
 
-  return <Table<Data> instance={instance} />
+  return <Table<Post> instance={instance} />
 
 }
