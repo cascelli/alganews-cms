225ac2be2845dcf3cdd879cf633cf6891592c434
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2021-12-16 15:06:01 -0300

    4.34. Entendendo como estruturar os dados no Chart

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,18 +1,18 @@
 import { Line } from 'react-chartjs-2';
 
 const data = {
-  labels: ['1', '2', '3', '4', '5', '6'],
+  labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'],
   datasets: [
     {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      label: 'Receitas',
+      data: [500, 400, 600, 100, 800, 20],
       fill: false,
       backgroundColor: 'rgb(255, 99, 132)',
       borderColor: 'rgba(255, 99, 132, 0.2)',
       yAxisID: 'y-axis-1',
     },
     {
-      label: '# of No Votes',
+      label: 'Despesas',
       data: [1, 2, 1, 1, 2, 2],
       fill: false,
       backgroundColor: 'rgb(54, 162, 235)',
