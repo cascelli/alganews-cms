01459d4417fa1c678e5853bce43363551c3d1d2d
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2021-12-18 15:14:52 -0300

    4.42. Documentando o CircleChart

--- src/stories/CircleChart.stories.tsx
+++ src/stories/CircleChart.stories.tsx
@@ -5,10 +5,22 @@ import CircleChart, { CircleChartProps } from '../components/CircleChart';
 export default {
   title: 'Example/CircleChart',
   component: CircleChart,
+  argTypes: {
+    progress: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 100,
+      }
+    }
+  }
 } as ComponentMeta<typeof CircleChart>;
 
 const Template: ComponentStory<typeof CircleChartProps> = (args) => <CircleChart {...args} />;
 
 export const Default = Template.bind({});
 
-Default.args = { };
+Default.args = { 
+  progress: 80,
+  size: 150
+};
