b55bc87a6c98d5f6cfd1ba4371fe3fc8b54eef26
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2021-12-20 14:39:13 -0300

    4.53. Desafio - ErrorDisplay

---
+++ src/stories/ErrorDisplay.stories.tsx
@@ -0,0 +1,43 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import ErrorDisplay, { ErrorDisplayProps } from '../components/ErrorDisplay';
+
+export default {
+  title: 'Example/ErrorDisplay',
+  component: ErrorDisplay,
+  argTypes: {
+    small: {
+      control: {
+        type: 'boolean'
+      }
+    }
+  }
+} as ComponentMeta<typeof ErrorDisplay>;
+
+const Template: ComponentStory<typeof ErrorDisplayProps> = (args) => 
+  <div>
+    <ErrorDisplay {...args} />
+  </div>
+
+export const Default = Template.bind({});
+Default.args = { };
+
+
+export const CustomTitle = Template.bind({})
+CustomTitle.args = {
+  title: 'Houve um erro'
+}
+
+
+export const CustomMessage = Template.bind({})
+CustomMessage.args = {
+  title: 'Houve um erro',
+  message: 'Falha na comunicação com o servidor'
+}
+
+
+export const Small = Template.bind({})
+Small.args = {
+  title: 'Houve um erro',
+  message: 'Falha na comunicação com o servidor',
+  small: true
+}
