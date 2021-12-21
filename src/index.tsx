b65ccef566dc0c237f1f9ffefab379918755b43b
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2021-12-21 20:00:24 -0300

    5.6. Quem é o Switch

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Notfound404 from './views/Notfound404.view';
+import Contact from './views/Contact.view';
+import Home from './views/Home.view';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Switch>
+
         <Route path={'/home'}>
-          <App />
+          <Home />
         </Route>
+
+        <Route path={'/contato'}>
+          <Contact />
+        </Route>
+
+        <Route>
+          <Notfound404 />
+        </Route>
+
       </Switch>
     </BrowserRouter>
   </React.StrictMode>,
---
+++ src/views/Notfound404.view.tsx
@@ -0,0 +1,5 @@
+export default function Notfound404() {
+  return <div>
+    <h1>Não encontrado - 404</h1>
+  </div>
+}
