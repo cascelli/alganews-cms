a26ca5c31668a6310b28cea999236e93fe7cb0fe
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2021-12-26 19:42:53 -0300

    6.7. Criando as primeiras Features

--- src/app/views/Home.view.tsx
+++ src/app/views/Home.view.tsx
@@ -1,11 +1,16 @@
 import usePageTitle from "../../core/Hooks/usePageTitle"
+import PostsList from "../features/PostsList"
+import UserMetrics from "../features/UserMetrics"
 import DefaultLayout from "../layouts/Default"
 
 export default function Home() {
   usePageTitle('Home')
   return <DefaultLayout>
-    <h1>Home</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis illo rem dolorum, magni blanditiis reprehenderit! Deserunt nihil velit pariatur beatae.</p>
 
+    <UserMetrics />
+
+    <PostsList />
+ 
   </DefaultLayout>
+  
 }
