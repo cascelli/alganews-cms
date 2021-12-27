db89338b8fa837e14faa0c0ceba518817040f91b
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2021-12-27 20:35:11 -0300

    6.22. Blindando a view com ErrorBoundary

--- src/app/features/EditorProfile.tsx
+++ src/app/features/EditorProfile.tsx
@@ -9,6 +9,10 @@ interface EditorProfileProps {
 }
 
 function EditorProfile (props: EditorProfileProps) {
+
+  // Descomentar para forçar erro e teste do ErrorBoundary
+  //throw new Error("Houve um erro ao renderizar o componente EditorProfile");
+
   return <EditorProfileWrapper>
     <EditorHeadline>
       <Avatar src={'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'} />
