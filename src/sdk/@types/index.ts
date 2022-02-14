b9aafc9a494c809c65f885302c74182023a758ae
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-02-14 11:24:13 -0300

    8.19. Criando FileService

--- src/sdk/@types/index.ts
+++ src/sdk/@types/index.ts
@@ -3,6 +3,7 @@ export type { Metric } from './Metric'
 export type { Payment } from './Payment'
 export type { Post } from './Post'
 export type { User } from './User'
+export type { File } from './File'
 
 // AlgaNews - para o caso de ter esquiecido algum type nas declaracoes acima
 export type { AlgaNews } from './AlgaNews'
