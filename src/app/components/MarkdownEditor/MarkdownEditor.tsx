6b6bba031eec46cf2da278b90b5528beffd01a45
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-02-20 11:41:01 -0300

    8.48. Abrindo links em uma nova aba

--- src/app/components/MarkdownEditor/MarkdownEditor.tsx
+++ src/app/components/MarkdownEditor/MarkdownEditor.tsx
@@ -7,6 +7,27 @@ MdEditor.unuse(Plugins.FontUnderline)
 
 const parser = new MarkdownIt()
 
+// Code Snippet para abrir hiperlinks para webpages existentes dentro de posts
+//  do MarkdownEditor em uma nova janela no navegador :
+// Obs : Testado na versão 12.0.1 do markdow-it (Veja no arquivo packages.json)
+//       e na versão 1.2.4 do react-markdown-editor-lite
+const defaultRender = parser.renderer.rules.link_open ||
+  function(tokens: any, idx: any, options: any, env: any, self: any) {
+    return self.renderToken(tokens, idx, options);
+  };
+
+parser.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  var aIndex = tokens[idx].attrIndex('target');
+  if (aIndex < 0) {
+    tokens[idx].attrPush(['target', '_blank']);
+  } else {
+    //@ts-ignore
+    tokens[idx].attrs[aIndex][1] = '_blank';
+  }
+  return defaultRender(tokens, idx, options, env, self);
+};
+
+
 export interface MarkdownEditorProps {
   onChange?: ( text: string) => any
   value?: string
