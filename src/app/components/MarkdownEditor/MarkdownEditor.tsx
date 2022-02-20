d6d3e86835fe256944a3c3819070a1394aeef996
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-02-20 09:53:53 -0300

    8.45. Renderizando markdown

--- src/app/components/MarkdownEditor/MarkdownEditor.tsx
+++ src/app/components/MarkdownEditor/MarkdownEditor.tsx
@@ -9,12 +9,21 @@ const parser = new MarkdownIt()
 
 export interface MarkdownEditorProps {
   onChange?: ( text: string) => any
+  value?: string
+  readOnly?: boolean
 }
 
 export default function MarkdownEditor (props: MarkdownEditorProps) {
   return <MdEditor
-    style={{ height: 300 }}
+    readOnly={props.readOnly}
+    style={{ height: props.readOnly ? 'auto' : 300 }}
+    value={props.value}
     renderHTML={ text => parser.render(text) }
     onChange ={ ({ text }) => props.onChange && props.onChange(text) }
+    view={props.readOnly ? {
+      menu: false, // Desabilita visualizacao do menu de edicao
+      md: false, // Desabilita propriedade de visualizacao do editor markdown (parte da edicao do post)
+      html: true // Habilita visualizacao de html do editor markdown (parte da visualizacao do post)
+    } : undefined }
   />
 }
