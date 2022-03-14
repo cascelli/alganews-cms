fbbb449eab37bdf963b6ce87ab3dbddb5770bcf7
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-03-14 18:58:00 -0300

    10.22. Desafio - Migrar features para os Hooks
    UsserPerformance.tsx

--- src/app/features/UserPerformance.tsx
+++ src/app/features/UserPerformance.tsx
@@ -1,13 +1,21 @@
-import { MetricService } from "danielbonifacio-sdk";
-import { useEffect, useState } from "react";
+//import { MetricService } from "danielbonifacio-sdk";
+//import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Skeleton from "react-loading-skeleton";
 import withBoundary from "../../core/hoc/withBoundary";
-import transformEditorMonthlyEaningsIntoChartJs from "../../core/utils/transformEditorMonthlyEarningsIntoChartJs";
-import Chart, { ChartProps } from "../components/Chart/Chart";
+import usePerformance from "../../core/hooks/usePerformance";
+//import transformEditorMonthlyEaningsIntoChartJs from "../../core/utils/transformEditorMonthlyEarningsIntoChartJs";
+//import Chart, { ChartProps } from "../components/Chart/Chart";
+import Chart from "../components/Chart/Chart";
+
 function UserPerformance() {
+  /*
   const [editorEarnings, setEditorEarings] = useState<ChartProps["data"]>();
   const [error, setError] = useState<Error>();
+  */
+  const { fetchPerformance, performance } = usePerformance();
 
+  /*
   useEffect(() => {
     MetricService.getEditorMonthlyEarnings()
       .then(transformEditorMonthlyEaningsIntoChartJs)
@@ -25,11 +33,23 @@ function UserPerformance() {
         <Skeleton height={227} />
       </div>
     );
+  */
+  useEffect(() => {
+    fetchPerformance();
+  }, [fetchPerformance]);
+
+  if (!performance)
+    return (
+      <div>
+        <Skeleton height={227} />
+      </div>
+    );
 
   return (
     <Chart
       title="Média de performance nos últimos 12 meses"
-      data={editorEarnings}
+      //data={editorEarnings}
+      data={performance}
     />
   );
 }
