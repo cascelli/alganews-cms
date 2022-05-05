ba8bf8f8c15c359d8cd71d94f05fc1fd71d22d27
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-05-05 14:25:58 -0300

    19.4. O primeiro teste unitário

--- src/tests/CircleChart.test.tsx
+++ src/tests/CircleChart.test.tsx
@@ -1,6 +1,15 @@
-import "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import CircleChart from "../app/components/CircleChart";
 
-test("true é truthy", () => {
-  const value = true;
-  expect(value).toBeTruthy();
+// test("true é truthy", () => {
+//   const value = true;
+//   expect(value).toBeTruthy();
+// });
+
+it("renderes caption", () => {
+  render(<CircleChart size={180} progress={80} caption={"javascript"} />);
+
+  const caption = screen.getByText("javascript");
+
+  expect(caption).toBeInTheDocument();
 });
