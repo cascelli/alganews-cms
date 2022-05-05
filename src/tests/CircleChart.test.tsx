f8695375d4e1ebf04e0c330fc46840568b50dd5e
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-05-05 15:01:11 -0300

    19.5. Testando a renderização de um componente

--- src/tests/CircleChart.test.tsx
+++ src/tests/CircleChart.test.tsx
@@ -13,3 +13,25 @@ it("renderes caption", () => {
 
   expect(caption).toBeInTheDocument();
 });
+
+it("renders percentage with % char", () => {
+  render(<CircleChart size={180} progress={80} caption={"javascript"} />);
+
+  const percentage = screen.getByText("80%");
+
+  expect(percentage).toBeVisible();
+});
+
+it("renders component with correct size", () => {
+  render(<CircleChart size={180} progress={80} caption={"javascript"} />);
+
+  const circleWrapper = screen.getByTestId("svg-wrapper");
+
+  expect(circleWrapper).toHaveStyle({ width: "180px", height: "180px" });
+});
+
+it("throws an error if progress is greater than 100", () => {
+  expect(() =>
+    render(<CircleChart size={180} progress={101} caption={"javascript"} />)
+  ).toThrowError();
+});
