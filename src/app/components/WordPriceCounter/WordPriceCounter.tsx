c696ea9ffd0575f3f8cd433ba1f3291d33c824a5
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-05-06 18:29:06 -0300

    20.6. Atualização - Lógica de singular e plural no WordPriceCounter

--- src/app/components/WordPriceCounter/WordPriceCounter.tsx
+++ src/app/components/WordPriceCounter/WordPriceCounter.tsx
@@ -1,22 +1,28 @@
-import * as WPC from './WordPriceCounter.styles'
+import * as WPC from "./WordPriceCounter.styles";
 
 export interface WordPriceCounterProps {
   wordsCount: number;
   pricePerWord: number;
 }
 
-function WordPriceCounter (props: WordPriceCounterProps) {
+function WordPriceCounter(props: WordPriceCounterProps) {
   if (props.wordsCount < 0)
-    throw Error('A quantidade de palavras não pode ser negativa')
+    throw Error("A quantidade de palavras não pode ser negativa");
 
-  return <WPC.Wrapper>
-    <WPC.WordCounter>{ props.wordsCount } palavras</WPC.WordCounter>
-    <WPC.PricePrivew>{(props.pricePerWord * props.wordsCount).toLocaleString('pt-br', {
-      style: 'currency',
-      currency: 'BRL',
-      maximumFractionDigits: 2
-    })}</WPC.PricePrivew>
-  </WPC.Wrapper>
+  return (
+    <WPC.Wrapper>
+      <WPC.WordCounter>
+        {props.wordsCount} {props.wordsCount === 1 ? "palavra" : "palavras"}
+      </WPC.WordCounter>
+      <WPC.PricePrivew>
+        {(props.pricePerWord * props.wordsCount).toLocaleString("pt-br", {
+          style: "currency",
+          currency: "BRL",
+          maximumFractionDigits: 2,
+        })}
+      </WPC.PricePrivew>
+    </WPC.Wrapper>
+  );
 }
 
-export default WordPriceCounter
+export default WordPriceCounter;
